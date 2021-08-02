86062858e29d0299a1f82cbbc3ff4a55ffb15a98
lcs-web <67989846+lcs-web@users.noreply.github.com>
2021-08-02 21:19:36 +0800

    Add files via upload

---
+++ 测试.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+
+using namespace cv;
+
+
+int main()
+{
+	// 读入一张图片（游戏原画）
+	Mat img = imread("锐雯.jpg");
+	// 创建一个名为 "游戏原画"窗口
+	namedWindow("游戏原画");
+	// 在窗口中显示游戏原画
+	imshow("游戏原画", img);
+	// 等待60000 ms后窗口自动关闭
+	waitKey(60000);
+	return 0;
+}
